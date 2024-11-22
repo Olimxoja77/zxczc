c20f0293f332e57ed136c519161bdeb0c10fca90
Olimxoja77 <odilxojaevolimxoja@gmail.com>
2024-11-22 14:54:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let a =document.querySelector('body')
+let b =document.querySelector('h1')
+let c =document.querySelector('p')
+let box2 =()=>{
+    a.style.background ='black'
+
+}
+let box =()=>{
+    a.style.background ='brown'
+        p.style.background ='white'
+      h1.style.background ='white'
+}
